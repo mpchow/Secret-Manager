7a4d183540ba226d0554f47605fa50cb455d0557
mpchow <mattchow918@gmail.com>
2022-10-07 18:04:10 -0700

    Initialize project

---
+++ src/test/java/com/example/secretmanager/SecretManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secretmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
